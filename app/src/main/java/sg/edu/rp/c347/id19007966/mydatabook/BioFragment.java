0d80e892ab652e9aa07296bd471c64645a3b3373
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-08 12:22:27 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/mydatabook/BioFragment.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.c347.id19007966.mydatabook;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BioFragment extends Fragment {
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bio, container, false);
+    }
+}
