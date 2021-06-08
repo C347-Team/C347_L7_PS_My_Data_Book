f8e69e5b353ffcd69cad7762aaad32932d5c7b7f
Mat <19024292@rp.edu.sg>
2021-06-08 13:40:28 +0800

    added drawer component to Main Activity. Created drawer_list.xml and updated activity_main.xml

--- app/src/main/java/sg/edu/rp/c347/id19007966/mydatabook/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/mydatabook/MainActivity.java
@@ -1,14 +1,72 @@
 package sg.edu.rp.c347.id19007966.mydatabook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] drawerItems;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    ArrayAdapter<String> aa;
+    String currentTitle;
+    ActionBar ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.left_drawer);
+
+        drawerItems = new String[]{"Bio", "Vaccination", "Anniversary", "About Us"};
+        ab = getSupportActionBar();
+
+        aa = new ArrayAdapter<String>(this,
+                R.layout.drawer_list, drawerItems);
+        drawerList.setAdapter(aa);
+
+        // Set the list's click listener
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int
+                    position, long arg3) {
+
+                Fragment fragment = null;
+                Intent i = null if (position == 0)
+                    fragment = new BioFragment();
+                else if (position == 1)
+                    fragment = new VaccinationFragment();
+                else if (position == 2)
+                    fragment = new AnniversaryFragment();
+                else if (position == 3) {
+                    i = new Intent(MainActivity.this, AboutUsActivity.class);
+                    startActivity(i);
+                }
+
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction trans = fm.beginTransaction();
+                trans.replace(R.id.content_frame, fragment);
+                trans.commit();
+
+                // Highlight the selected item,
+                //  update the title, and close the drawer
+                drawerList.setItemChecked(position, true);
+                currentTitle = drawerItems[position];
+                ab.setTitle(currentTitle);
+                drawerLayout.closeDrawer(drawerList);
+            }
+        });
     }
 }
